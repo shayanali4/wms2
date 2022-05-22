965c98381f95f0ee8177c35cff18caa885901c29
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-05-22 14:55:24 +0100

    notStartedDataPullingThrough

--- components/Table/NotStarted.tsx
+++ components/Table/NotStarted.tsx
@@ -1,100 +1,126 @@
-import TableTemplate from '../Table';
-import newOrders from '../../data/newOrders';
+import workOrders from '../../data/workOrders';
+import brands from '../../data/brands';
+import workers from '../../data/workers';
+import { NotStartedObject } from '../../interfaces/NotStartedObject';
 
-type Props = {};
+type Props = { orders: NotStartedObject };
 
-let orderObj = newOrders;
-console.log(orderObj);
+const workOrderData = workOrders;
 
-const NotStartedTable: React.FunctionComponent<Props> = ({}) => (
-  <TableTemplate
-    orderObject={orderObj}
-    headers={[
-      {
-        title: 'Time Accepted',
-        objKey: 'time_accepted',
-        alignHeader: 'left',
-        field: {
-          type: 'dateTime',
-          props: { bold: false, align: 'left' },
-        },
-      },
-      {
-        title: 'ORDER #',
-        alignHeader: 'left',
-        objKey: 'tracking_id',
-        field: { type: 'text', props: { bold: true, align: 'left' } },
-      },
-      {
-        title: 'Task',
-        alignHeader: 'left',
-        objKey: 'work_order_name',
-        field: {
-          type: 'text',
-          props: { bold: true, align: 'center' },
-        },
-      },
-      {
-        title: 'Units/Quantity',
-        alignHeader: 'center',
-        objKey: 'work_order_name',
-        field: {
-          type: 'number',
-          props: { bold: false, align: 'center' },
-        },
-      },
-      {
-        title: 'Brand',
-        alignHeader: 'center',
-        objKey: 'brand_system',
-        field: {
-          type: 'text',
-          props: { bold: false, align: 'center' },
-        },
-      },
-      {
-        title: 'Target Time',
-        alignHeader: 'center',
-        objKey: 'target_time',
-        field: {
-          type: 'text',
-          props: { bold: false, align: 'center' },
-        },
-      },
-      {
-        title: 'Initial Cost',
-        alignHeader: 'center',
-        objKey: 'cost_initial',
-        field: {
-          type: 'number',
-          props: { bold: false, align: 'center' },
-        },
-      },
-      {
-        title: 'Assigned To',
-        alignHeader: 'center',
-        objKey: 'assigned_to',
-        field: {
-          type: 'text',
-          props: { bold: false, align: 'center' },
-        },
-      },
-      {
-        title: 'Start Order',
-        alignHeader: 'center',
-        objKey: 'tracking_id',
-        field: {
-          type: 'text',
-          props: {
-            value: 'Update',
-            align: 'center',
-            color: 'bg-yellow-300',
-          },
-        },
-      },
-    ]}
-    colors={{ table: 'bg-yellow-300', text: 'text-gray-600' }}
-  />
+const NotStartedTable: React.FunctionComponent<Props> = ({
+  orders,
+}) => (
+  <table className="min-w-max w-full table-auto">
+    <thead>
+      <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+        <th className="py-3 px-6 text-left">Time Accepted</th>
+        <th className="py-3 px-6 text-left">ID</th>
+        <th className="py-3 px-6 text-center">Task</th>
+        <th className="py-3 px-6 text-center">Units / Quantity</th>
+        <th className="py-3 px-6 text-center">Brand (Actual)</th>
+        <th className="py-3 px-6 text-center">Target Time</th>
+        <th className="py-3 px-6 text-center">Initial Cost</th>
+        <th className="py-3 px-6 text-center">Assigned To</th>
+        <th className="py-3 px-6 text-center">Start Order</th>
+      </tr>
+    </thead>
+    <tbody className="text-gray-600 text-sm font-light">
+      {orders
+        ? orders.map((order) => {
+            return (
+              <tr
+                key={order.tracking_id}
+                className="border-b border-gray-200 hover:bg-gray-100"
+              >
+                <td className="py-3 px-6 text-left">
+                  <div className="flex items-center">
+                    <span>
+                      {order.time_accepted
+                        ? order.time_accepted
+                            .slice(0, 19)
+                            .replace(/T/g, ' ')
+                        : null}
+                    </span>
+                  </div>
+                </td>
+                <td className="py-3 px-6 text-left whitespace-nowrap">
+                  <div className="flex items-center">
+                    <span className="font-medium">
+                      {order.tracking_id}
+                    </span>
+                  </div>
+                </td>
+                <td className="py-3 px-6 text-center">
+                  <div className="flex items-center justify-center">
+                    <span>
+                      {workOrderData.filter(
+                        (wo) => wo.id === order.id
+                      )[0]
+                        ? workOrderData.filter(
+                            (wo) => wo.id === order.id
+                          )[0].name
+                        : 'null'}
+                    </span>
+                  </div>
+                </td>
+                <td className="py-3 px-6 text-center">
+                  <span className="font-medium">
+                    {order.initial_units_or_quantity}
+                  </span>
+                </td>
+                <td className="py-3 px-6 text-center">
+                  <div className="flex items-center justify-center">
+                    <span>
+                      {
+                        brands.filter(
+                          (brand) => brand.id === order.brand_id
+                        )[0].name
+                      }
+                    </span>
+                  </div>
+                </td>
+                <td className="py-3 px-6 text-center">
+                  <div className="flex items-center justify-center">
+                    {console.log(order.target_time)}
+                    <span>{order.target_time} minutes</span>
+                  </div>
+                </td>
+                <td className="py-3 px-6 text-center">
+                  <div className="flex items-center justify-center">
+                    <span>£{order.initial_cost}</span>
+                  </div>
+                </td>
+                <td className="py-3 px-6 text-center">
+                  <div className="flex items-center justify-center">
+                    {console.log(order.assigned_to)}
+                    <span>
+                      {workers.filter(
+                        (worker) => worker.id === order.assigned_to
+                      )[0]
+                        ? workers.filter(
+                            (worker) =>
+                              worker.id === order.assigned_to
+                          )[0].name
+                        : null}
+                    </span>
+                  </div>
+                </td>
+                <td className="py-3 px-6 text-center">
+                  <div className="flex item-center justify-center">
+                    <button
+                      className="px-3 py-1 bg-blue-600 rounded-md text-white outline-none focus:ring-4 shadow-lg transform active:scale-75 transition-transform"
+                      value={order.id}
+                    >
+                      Update
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            );
+          })
+        : null}
+    </tbody>
+  </table>
 );
 
 export default NotStartedTable;
