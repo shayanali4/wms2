73127c029bf544040182c147ef55e4c2718d5b6a
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-06-14 09:29:48 +0100

    Finish Page base layer

---
+++ pages/finish/[id].tsx
@@ -0,0 +1,100 @@
+import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+import Layout from '../../components/Layout';
+import { FinishSummary } from '../../components/WorkOrderScreens/Finish/FinishSummary';
+import { SpecificDetails } from '../../components/WorkOrderScreens/SpecificDetails';
+import { EstimatedCosts } from '../../components/WorkOrderScreens/EstimatedCosts';
+import { ActionWO } from '../../components/WorkOrderScreens/AcceptorReject/ActionWO';
+import { supabaseClient } from '../../lib/client';
+
+const FinishIndex: NextPage = (props) => {
+  // const [loading, setLoading] = useState(true);
+  const [workOrder, setWorkOrder] = useState({});
+  const [specifics, setSpecifics] = useState({});
+  const [task, setTask] = useState({});
+  const [brands, setBrands] = useState({});
+
+  useEffect(() => {
+    const fetchNewOrders = async () => {
+      console.log(props.id);
+
+      const getWorkOrder = async () => {
+        const { data } = await supabaseClient
+          .from('order')
+          .select('*')
+          .eq('id', props.id)
+          .single();
+        console.log(data);
+        setWorkOrder(data || {});
+      };
+      const getSpecifics = async () => {
+        const { data } = await supabaseClient
+          .from('specific_fields')
+          .select('*')
+          .eq('order_id', props.id)
+          .single();
+        console.log(data);
+        setSpecifics(data || {});
+      };
+      const getWorkTask = async () => {
+        console.log(workOrder.work_order_id);
+        const { data } = await supabaseClient
+          .from('work_tasks')
+          .select('*')
+          .eq('id', Number(workOrder.work_order_id))
+          .single();
+
+        console.log(data);
+        setTask(data || {});
+      };
+      const getBrands = async () => {
+        const { data } = await supabaseClient
+          .from('brands')
+          .select('*');
+        console.log(data);
+        setBrands(data || {});
+      };
+      getWorkOrder();
+      getSpecifics();
+      getWorkTask();
+      getBrands();
+    };
+    fetchNewOrders().catch(console.error);
+    return () => {
+      setWorkOrder({}); // Clean up
+      setSpecifics({}); // Clean up
+      setTask({}); // Clean up
+      setBrands({}); // Clean up
+    };
+  }, []);
+
+  return (
+    <>
+      <Layout title={`Complete WO`} />
+      <FinishSummary workOrder={workOrder} task={task} />
+      {'----'}
+      <SpecificDetails specifics={specifics} workOrder={workOrder} />
+      {'----'}
+      {/* add form here */}
+      <EstimatedCosts
+        task={task}
+        workOrder={workOrder}
+        brands={brands}
+      />
+      {'----'}
+      <ActionWO />
+    </>
+  );
+};
+
+export default FinishIndex;
+
+export async function getServerSideProps(context: any) {
+  const id = context.query.id;
+  console.log('Finish SSPid: ', id);
+  return {
+    props: {
+      id,
+    },
+  };
+}
