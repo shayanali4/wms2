7c4b2469f8b5cf81a046851ca84b07592d39d225
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-06-25 17:56:32 +0100

    declinedTableWIP

--- components/Table/Views/index.tsx
+++ components/Table/Views/index.tsx
@@ -2,8 +2,10 @@ import WIPTable from './WIP';
 import NotStartedTable from './NotStarted';
 import NewOrderTable from './NewOrders';
 import CompletedTable from './Completed';
+import DeclinedTable from './Declined';
 
 export const Wip = WIPTable;
 export const NotStarted = NotStartedTable;
 export const NewOrder = NewOrderTable;
 export const Completed = CompletedTable;
+export const Declined = DeclinedTable;
