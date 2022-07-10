abc4a8ec9c5f78de4f74ce68d89d0fcae2d1aa5a
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-07-10 14:36:09 +0100

    CancelledTablePullingThrough

--- pages/cancelled.tsx
+++ pages/cancelled.tsx
@@ -6,13 +6,13 @@ import CancelledTable from '../components/Table/Views/Cancelled';
 
 const CancelledPage: NextPage = () => {
   const [orders, setOrders] = useState([{}]);
-  const [tasks, setWorkTasks] = useState([{}]);
+  const [workTasks, setWorkTasks] = useState([{}]);
 
   useEffect(() => {
     let mounted = true;
     fetchOrdersTrackerStatus(99).then((data: any) => {
-      setOrders(data.orders || [{}]);
-      setWorkTasks(data.workTasks || [{}]);
+      setOrders(data.orders);
+      setWorkTasks(data.workTasks);
     });
     return () => {
       mounted = false;
@@ -23,7 +23,7 @@ const CancelledPage: NextPage = () => {
     <>
       <Page layoutTitle="Completed Orders | Work Management System | TuPack">
         {orders ? (
-          <CancelledTable orders={orders} tasks={tasks} />
+          <CancelledTable orders={orders} workTasks={workTasks} />
         ) : (
           <div>Loading Table...</div>
         )}
