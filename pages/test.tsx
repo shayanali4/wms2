58b428e8015f70cc637423d98c67833a2b49c22a
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-07-28 18:39:11 +0100

    emailUpdate

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,19 +1,19 @@
-import axios from "axios";
-import React from "react";
+import axios from 'axios';
+import React from 'react';
 
 function Test() {
   const sendMail = async () => {
     try {
       //   setMessage("Sending test mail");
 
-      const { data } = await axios.post("/api/send-email", {
-        to: "shayana803@gmail.com",
-        subject: "Test Transactional Email",
-        emailBody: "<h1>This is html inside transactional email</h1>",
+      const { data } = await axios.post('/api/send-email', {
+        to: 'edgeorge101@gmail.com',
+        subject: 'Test Transactional Email',
+        emailBody: '<h1>This is html inside transactional email</h1>',
       });
 
       if (data.ok) {
-        alert("Successfully send test mail");
+        alert('Successfully send test mail');
       }
     } catch (error) {
       console.log(error);
