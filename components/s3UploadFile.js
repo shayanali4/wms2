260a596fc8dd56490abdba984ce46da2ea0271de
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-08-04 14:24:57 +0100

    .env variables updated

--- components/s3UploadFile.js
+++ components/s3UploadFile.js
@@ -1,9 +1,9 @@
 import AWS from 'aws-sdk';
 
 const S3_BUCKET = 'wmspics';
-const REGION = 'eu-west-2';
-const accessKeyId = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY;
+const REGION = process.env.LOCAL_AWS_REGION || 'eu-west-2';
+const accessKeyId = process.env.LOCAL_AWS_ACCESS_KEY;
+const secretAccessKey = process.env.LOCAL_AWS_SECRET_ACCESS_KEY;
 
 AWS.config.update({
   accessKeyId: accessKeyId,
